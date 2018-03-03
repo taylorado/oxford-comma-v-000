31e82d62c96810e9b8f4049fb98696c750131609
Taylor Donlan <taylordonlan@gmail.com>
2018-03-03 19:39:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
  elsif array.length == 2
    return "#{array[0]} and #{array[1]}"
  else
-   array[array.length-1] << "and"
+   array[array.length-2] << "and"
    "#{array[0, array.length].join(', ')} and #{array.last}"
  end
 end
